5a1422a218efc69ef20c27e566ea077e09aded14
Giffari Alfarizy <giffarialfarizy@gmail.com>
2022-04-20 11:34:28 +0700

    🚚 Folder structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 
 const Home: NextPage = () => {
 	return (
-		<div className='blog-container'>
+		<div className='container'>
 			<Head>
 				<title>Home</title>
 				<meta
@@ -15,42 +15,45 @@ const Home: NextPage = () => {
 
 			<main>
 				<h1>Giffari Alfarizy</h1>
-				<h2>Title</h2>
-				<p className='paragraph'>
-					Let’s talk about the future.
-				</p>
-				<p className='paragraph'>
-					I like the idea of decentralization amongst
-					nations and want all countries to be as autonomous
-					as possible; however, let’s say that we enter
-					interstellar space and we discover there are
-					hundreds of alien civilizations.
-				</p>
-				<p className='paragraph'>
-					Well, I don’t know about you but *someone* on
-					Earth needs to be in charge and it sure as hell
-					isn’t going to be a planet full-on individualism!
-				</p>
-				<p className='paragraph'>
-					In order to present a respectable image in the
-					galactic playing field, we would need to represent
-					our planet and our species, not our particular
-					island.
-				</p>
-				<p className='paragraph'>
-					And this is where Tesla CEO Elon Musk sees
-					humanity’s biggest problem. Musk believes that by
-					the time we realize we should pool our resources
-					and unite, it’ll be all too late
-				</p>
+				<section className='blog'>
+					<h2>Title</h2>
+					<p className='paragraph'>
+						Let’s talk about the future.
+					</p>
+					<p className='paragraph'>
+						I like the idea of decentralization amongst
+						nations and want all countries to be as
+						autonomous as possible; however, let’s say that
+						we enter interstellar space and we discover
+						there are hundreds of alien civilizations.
+					</p>
+					<p className='paragraph'>
+						Well, I don’t know about you but *someone* on
+						Earth needs to be in charge and it sure as hell
+						isn’t going to be a planet full-on
+						individualism!
+					</p>
+					<p className='paragraph'>
+						In order to present a respectable image in the
+						galactic playing field, we would need to
+						represent our planet and our species, not our
+						particular island.
+					</p>
+					<p className='paragraph'>
+						And this is where Tesla CEO Elon Musk sees
+						humanity’s biggest problem. Musk believes that
+						by the time we realize we should pool our
+						resources and unite, it’ll be all too late
+					</p>
 
-				<p className='paragraph'>
-					“I wonder how many dead ‘one planet’ civilizations
-					there are out there in the cosmos? Species that
-					never made it to another planet and ultimately
-					extinguished themselves or were destroyed by
-					external factors. Probably a few.”
-				</p>
+					<p className='paragraph'>
+						“I wonder how many dead ‘one planet’
+						civilizations there are out there in the cosmos?
+						Species that never made it to another planet and
+						ultimately extinguished themselves or were
+						destroyed by external factors. Probably a few.”
+					</p>
+				</section>
 
 				<div className='error'>Error message</div>
 				<div className='success'>Success message</div>
